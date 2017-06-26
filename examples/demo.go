HarrisonATT/yanff

Add cmd line options to set port numbers in tests and examples

examples/demo.go
@@ -11,13 +11,22 @@ import "github.com/intel-go/yanff/rules"
 import "flag"
 import "time"
 
-var L2Rules *rules.L2Rules
-var L3Rules *rules.L3Rules
-var load uint
-var options = `{"cores": {"Value": 16, "Locked": false}}`
+var (
+	L2Rules *rules.L2Rules
+	L3Rules *rules.L3Rules
+	load    uint
+	options = `{"cores": {"Value": 16, "Locked": false}}`
+
+	RECV_PORT  uint
+	SEND_PORT1 uint
+	SEND_PORT2 uint
+)
 
 func main() {
 	flag.UintVar(&load, "load", 1000, "Use this for regulating 'load intensity', number of iterations")
+	flag.UintVar(&RECV_PORT, "RECV_PORT", 0, "port for receiver")
+	flag.UintVar(&SEND_PORT1, "SEND_PORT1", 1, "port for 1st sender")
+	flag.UintVar(&SEND_PORT2, "SEND_PORT2", 2, "port for 2nd sender")
 
 	// Initialize YANFF library at requested number of cores
 	flow.SystemInit(options)
@@ -28,7 +37,7 @@ func main() {
 	go updateSeparateRules()
 
 	// Receive packets from zero port. One queue will be added automatically.
-	firstFlow := flow.SetReceiver(0)
+	firstFlow := flow.SetReceiver(uint8(RECV_PORT))
 
 	// Separate packets for additional flow due to some rules
 	secondFlow := flow.SetSeparator(firstFlow, L3Separator)
@@ -37,8 +46,8 @@ func main() {
 	flow.SetHandler(firstFlow, heavyFunc)
 
 	// Send both flows each one to one port. Queues will be added automatically.
-	flow.SetSender(firstFlow, 1)
-	flow.SetSender(secondFlow, 2)
+	flow.SetSender(firstFlow, uint8(SEND_PORT1))
+	flow.SetSender(secondFlow, uint8(SEND_PORT2))
 
 	flow.SystemStart()
 }

test/performance/perf_main.go
@@ -9,21 +9,33 @@ import "github.com/intel-go/yanff/packet"
 
 import "flag"
 
-var load uint
-var loadRW uint
-var cores uint
-var options = `{"cores": {"Value": 35, "Locked": false}}`
+var (
+	load    uint
+	loadRW  uint
+	cores   uint
+	options = `{"cores": {"Value": 35, "Locked": false}}`
+
+	RECV_PORT1 uint
+	RECV_PORT2 uint
+	SEND_PORT1 uint
+	SEND_PORT2 uint
+)
 
 func main() {
 	flag.UintVar(&load, "load", 1000, "Use this for regulating 'load intensity', number of iterations")
 	flag.UintVar(&loadRW, "loadRW", 50, "Use this for regulating 'load read/write intensity', number of iterations")
 
+	flag.UintVar(&SEND_PORT1, "SEND_PORT1", 1, "port for 1st sender")
+	flag.UintVar(&SEND_PORT2, "SEND_PORT2", 1, "port for 2nd sender")
+	flag.UintVar(&RECV_PORT1, "RECV_PORT1", 0, "port for 1st receiver")
+	flag.UintVar(&RECV_PORT2, "RECV_PORT2", 0, "port for 2nd receiver")
+
 	// Initialize YANFF library at requested number of cores
 	flow.SystemInit(options)
 
 	// Receive packets from zero port. One queue per receive will be added automatically.
-	firstFlow0 := flow.SetReceiver(0)
-	firstFlow1 := flow.SetReceiver(0)
+	firstFlow0 := flow.SetReceiver(uint8(RECV_PORT1))
+	firstFlow1 := flow.SetReceiver(uint8(RECV_PORT2))
 
 	firstFlow := flow.SetMerger(firstFlow0, firstFlow1)
 
@@ -32,8 +44,9 @@ func main() {
 
 	// Split for two senders and send
 	secondFlow := flow.SetPartitioner(firstFlow, 150, 150)
-	flow.SetSender(firstFlow, 1)
-	flow.SetSender(secondFlow, 1)
+
+	flow.SetSender(firstFlow, uint8(SEND_PORT1))
+	flow.SetSender(secondFlow, uint8(SEND_PORT2))
 
 	flow.SystemStart()
 }

test/stability/test1/part1.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"sync"
 	"sync/atomic"
@@ -31,6 +32,9 @@ var (
 	receivedPackets uint64     = 0
 	testDoneEvent   *sync.Cond = nil
 	passed          int32      = 1
+
+	SEND_PORT uint
+	RECV_PORT uint
 )
 
 // This part of test generates packets on port 0 and receives them on
@@ -42,6 +46,9 @@ var (
 // calculates sent/received ratio and prints it when a predefined
 // number of packets is received.
 func main() {
+	flag.UintVar(&SEND_PORT, "SEND_PORT", 0, "port for sender")
+	flag.UintVar(&RECV_PORT, "RECV_PORT", 1, "port for receiver")
+
 	// Init YANFF system at 16 available cores
 	flow.SystemInit(options)
 
@@ -51,10 +58,10 @@ func main() {
 	// Create packets with speed at least 1000 packets/s
 	firstFlow := flow.SetGenerator(generatePacket, 1000)
 	// Send all generated packets to the output
-	flow.SetSender(firstFlow, 0)
+	flow.SetSender(firstFlow, uint8(SEND_PORT))
 
 	// Create receiving flow and set a checking function for it
-	secondFlow := flow.SetReceiver(1)
+	secondFlow := flow.SetReceiver(uint8(RECV_PORT))
 	flow.SetHandler(secondFlow, checkPackets)
 	flow.SetStopper(secondFlow)
 

aregm/nff-go

examples/demo.go
@@ -11,12 +11,21 @@ import "github.com/intel-go/yanff/rules"
 import "flag"
 import "time"
 
-var L2Rules *rules.L2Rules
-var L3Rules *rules.L3Rules
-var load uint
+var (
+	L2Rules *rules.L2Rules
+	L3Rules *rules.L3Rules
+	load    uint
+
+	inport  uint
+	outport1 uint
+	outport2 uint
+)
 
 func main() {
 	flag.UintVar(&load, "load", 1000, "Use this for regulating 'load intensity', number of iterations")
+	flag.UintVar(&inport, "inport", 0, "port for receiver")
+	flag.UintVar(&outport1, "outport1", 1, "port for 1st sender")
+	flag.UintVar(&outport2, "outport2", 2, "port for 2nd sender")
 
 	// Initialize YANFF library at 16 cores by default
 	flow.SystemInit(16)
@@ -27,7 +36,7 @@ func main() {
 	go updateSeparateRules()
 
 	// Receive packets from zero port. One queue will be added automatically.
-	firstFlow := flow.SetReceiver(0)
+	firstFlow := flow.SetReceiver(uint8(inport))
 
 	// Separate packets for additional flow due to some rules
 	secondFlow := flow.SetSeparator(firstFlow, L3Separator, nil)
@@ -36,8 +45,8 @@ func main() {
 	flow.SetHandler(firstFlow, heavyFunc, nil)
 
 	// Send both flows each one to one port. Queues will be added automatically.
-	flow.SetSender(firstFlow, 1)
-	flow.SetSender(secondFlow, 2)
+	flow.SetSender(firstFlow, uint8(outport1))
+	flow.SetSender(secondFlow, uint8(outport2))
 
 	flow.SystemStart()
 }

test/performance/perf_main.go
@@ -9,19 +9,31 @@ import "github.com/intel-go/yanff/packet"
 
 import "flag"
 
-var load uint
-var loadRW uint
+var (
+	load   uint
+	loadRW uint
+
+	inport1 uint
+	inport2 uint
+	outport1 uint
+	outport2 uint
+)
 
 func main() {
 	flag.UintVar(&load, "load", 1000, "Use this for regulating 'load intensity', number of iterations")
 	flag.UintVar(&loadRW, "loadRW", 50, "Use this for regulating 'load read/write intensity', number of iterations")
 
+	flag.UintVar(&outport1, "outport1", 1, "port for 1st sender")
+	flag.UintVar(&outport2, "outport2", 1, "port for 2nd sender")
+	flag.UintVar(&inport1, "inport1", 0, "port for 1st receiver")
+	flag.UintVar(&inport2, "inport2", 0, "port for 2nd receiver")
+
 	// Initialize YANFF library at 35 cores by default
 	flow.SystemInit(35)
 
 	// Receive packets from zero port. One queue per receive will be added automatically.
-	firstFlow0 := flow.SetReceiver(0)
-	firstFlow1 := flow.SetReceiver(0)
+	firstFlow0 := flow.SetReceiver(uint8(inport1))
+	firstFlow1 := flow.SetReceiver(uint8(inport2))
 
 	firstFlow := flow.SetMerger(firstFlow0, firstFlow1)
 
@@ -30,8 +42,9 @@ func main() {
 
 	// Split for two senders and send
 	secondFlow := flow.SetPartitioner(firstFlow, 150, 150)
-	flow.SetSender(firstFlow, 1)
-	flow.SetSender(secondFlow, 1)
+
+	flow.SetSender(firstFlow, uint8(outport1))
+	flow.SetSender(secondFlow, uint8(outport2))
 
 	flow.SystemStart()
 }

test/stability/test1/part1.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"sync"
 	"sync/atomic"
@@ -30,6 +31,9 @@ var (
 	receivedPackets uint64     = 0
 	testDoneEvent   *sync.Cond = nil
 	passed          int32      = 1
+
+	outport uint
+	inport uint
 )
 
 // This part of test generates packets on port 0 and receives them on
@@ -41,6 +45,9 @@ var (
 // calculates sent/received ratio and prints it when a predefined
 // number of packets is received.
 func main() {
+	flag.UintVar(&outport, "outport", 0, "port for sender")
+	flag.UintVar(&inport, "inport", 1, "port for receiver")
+
 	// Init YANFF system at 16 available cores
 	flow.SystemInit(16)
 
@@ -50,10 +57,10 @@ func main() {
 	// Create packets with speed at least 1000 packets/s
 	firstFlow := flow.SetGenerator(generatePacket, 1000, nil)
 	// Send all generated packets to the output
-	flow.SetSender(firstFlow, 0)
+	flow.SetSender(firstFlow, uint8(outport))
 
 	// Create receiving flow and set a checking function for it
-	secondFlow := flow.SetReceiver(1)
+	secondFlow := flow.SetReceiver(uint8(inport))
 	flow.SetHandler(secondFlow, checkPackets, nil)
 	flow.SetStopper(secondFlow)
 