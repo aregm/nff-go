Shpigor/yanff

Implemented checksum calculations in SW and HW offloading

low/low.go
@@ -12,7 +12,8 @@ extern void recv(uint8_t port, uint16_t queue, struct rte_ring *, uint8_t coreId
 extern void send(uint8_t port, uint16_t queue, struct rte_ring *, uint8_t coreId);
 extern void stop(struct rte_ring *);
 extern void initCPUSet(uint8_t coreId, cpu_set_t* cpuset);
-extern int port_init(uint8_t port, uint16_t receiveQueuesNumber, uint16_t sendQueuesNumber, struct rte_mempool *mbuf_pool, struct ether_addr *addr);
+extern int port_init(uint8_t port, uint16_t receiveQueuesNumber, uint16_t sendQueuesNumber, struct rte_mempool *mbuf_pool,
+    struct ether_addr *addr, bool hwtxchecksum);
 extern struct rte_mempool * createMempool(int portsNumber, uint32_t num_mbuf, uint32_t mbuf_cache_size);
 extern int directStop(int pkts_for_free_number, struct rte_mbuf ** buf);
 extern char ** makeArgv(int n);
@@ -105,6 +106,71 @@ func TrimMbuf(m *Mbuf, length uint) bool {
 	m.data_len -= C.uint16_t(length)
 	m.pkt_len -= C.uint32_t(length)
 	return true
+}
+
+func SetTXIPv4OLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_IP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (1 << 54) | (1 << 55)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv4UDPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_UDP_CKSUM | PKT_TX_IP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (3 << 52) | (1 << 54) | (1 << 55)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv4TCPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_TCP_CKSUM | PKT_TX_IP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (1 << 52) | (1 << 54) | (1 << 55)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv6UDPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_UDP_CKSUM | PKT_TX_IPV6
+	mb.ol_flags = (3 << 52) | (1 << 56)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv6TCPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_TCP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (1 << 52) | (1 << 56)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
 }
 
 // These constants are used by packet package to parse protocol headers
@@ -388,10 +454,10 @@ func GetPortsNumber() int {
 	return int(C.rte_eth_dev_count())
 }
 
-func CreatePort(port uint8, receiveQueuesNumber uint16, sendQueuesNumber uint16) {
+func CreatePort(port uint8, receiveQueuesNumber uint16, sendQueuesNumber uint16, hwtxchecksum bool) {
 	addr := make([]byte, C.ETHER_ADDR_LEN)
 	if C.port_init(C.uint8_t(port), C.uint16_t(receiveQueuesNumber), C.uint16_t(sendQueuesNumber),
-		mainMempool, (*C.struct_ether_addr)(unsafe.Pointer(&(addr[0])))) != 0 {
+		mainMempool, (*C.struct_ether_addr)(unsafe.Pointer(&(addr[0]))), C._Bool(hwtxchecksum)) != 0 {
 		common.LogError(common.Initialization, "Cannot init port ", port, "!")
 	}
 	t := hex.Dump(addr)

test/stability/test_cksum/part2.go
@@ -1,0 +1,69 @@
+// Copyright 2017 Intel Corporation.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/intel-go/yanff/flow"
+	"github.com/intel-go/yanff/packet"
+
+	"github.com/intel-go/yanff/test/stability/test_cksum/common"
+)
+
+var hwol bool
+
+// Main function for constructing packet processing graph.
+func main() {
+	var inport, outport uint
+
+	flag.BoolVar(&hwol, "hwol", false, "Use Hardware offloading for TX checksums calculation")
+	flag.UintVar(&inport, "inport", 0, "Input port number")
+	flag.UintVar(&outport, "outport", 1, "Output port number")
+
+	// Init YANFF system
+	config := flow.Config {
+		CPUCoresNumber: 16,
+		HWTXChecksum: hwol,
+	}
+	flow.SystemInit(&config)
+
+	// Receive packets from zero port. Receive queue will be added automatically.
+	inputFlow := flow.SetReceiver(uint8(inport))
+	flow.SetHandler(inputFlow, fixPacket, nil)
+	flow.SetSender(inputFlow, uint8(outport))
+
+	// Begin to process packets.
+	flow.SystemStart()
+}
+
+func fixPacket(pkt *packet.Packet, context flow.UserContext) {
+	if !common.CheckPacketChecksums(pkt) {
+		println("TEST FAILED")
+	}
+
+	offset := pkt.ParseL4Data()
+	if offset < 0 {
+		println("ParseL4 returned negative value", offset)
+		println("TEST FAILED")
+		return
+	}
+
+	ptr := (*common.Packetdata)(pkt.Data)
+	if ptr.F2 != 0 {
+		fmt.Printf("Bad data found in the packet: %x\n", ptr.F2)
+		println("TEST FAILED")
+		return
+	}
+
+	ptr.F2 = ptr.F1
+
+	if hwol {
+		packet.SetHWCksumOLFlags(pkt)
+	} else {
+		common.CalculateChecksum(pkt)
+	}
+}

test/stability/test_cksum/common/test1_common.go
@@ -1,0 +1,92 @@
+// Copyright 2017 Intel Corporation.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package common
+
+import (
+	"github.com/intel-go/yanff/packet"
+)
+
+type Packetdata struct {
+	F1, F2 uint64
+}
+
+func CheckPacketChecksums(p *packet.Packet) bool {
+	status := false
+
+	if p.Ether.EtherType == packet.SwapBytesUint16(packet.IPV4Number) {
+		l3status := true
+		if p.IPv4.HdrChecksum != packet.CalculateIPv4Checksum(p) {
+			println("IPv4 checksum mismatch")
+			l3status = false
+		}
+
+		if p.IPv4.NextProtoID == packet.UDPNumber {
+			csum := packet.CalculateIPv4UDPChecksum(p)
+			if p.UDP.DgramCksum != csum {
+				println("IPv4 UDP datagram checksum mismatch", p.UDP.DgramCksum, "should be", csum)
+			} else {
+				status = l3status
+			}
+		} else if p.IPv4.NextProtoID == packet.TCPNumber {
+			csum := packet.CalculateIPv4TCPChecksum(p)
+			if p.TCP.Cksum != csum {
+				println("IPv4 TCP checksum mismatch", p.TCP.Cksum, "should be", csum)
+			} else {
+				status = l3status
+			}
+		} else {
+			println("Unknown IPv4 protocol number", p.IPv4.NextProtoID)
+		}
+	} else if p.Ether.EtherType == packet.SwapBytesUint16(packet.IPV6Number) {
+		if p.IPv6.Proto == packet.UDPNumber {
+			csum := packet.CalculateIPv6UDPChecksum(p)
+			if p.UDP.DgramCksum != csum {
+				println("IPv6 UDP datagram checksum mismatch:", p.UDP.DgramCksum, "should be", csum)
+			} else {
+				status = true
+			}
+		} else if p.IPv6.Proto == packet.TCPNumber {
+			csum := packet.CalculateIPv6TCPChecksum(p)
+			if p.TCP.Cksum != csum {
+				println("IPv6 TCP datagram checksum mismatch", p.TCP.Cksum, "should be", csum)
+			} else {
+				status = true
+			}
+		} else {
+			println("Unknown IPv6 protocol number", p.IPv6.Proto)
+		}
+	} else {
+		println("Unknown packet EtherType", p.Ether.EtherType)
+	}
+
+	return status
+}
+
+func CalculateChecksum(p *packet.Packet) {
+	if p.Ether.EtherType == packet.SwapBytesUint16(packet.IPV4Number) {
+		p.IPv4.HdrChecksum = packet.CalculateIPv4Checksum(p)
+
+		if p.IPv4.NextProtoID == packet.UDPNumber {
+			p.UDP.DgramCksum = packet.CalculateIPv4UDPChecksum(p)
+		} else if p.IPv4.NextProtoID == packet.TCPNumber {
+			p.TCP.Cksum = packet.CalculateIPv4TCPChecksum(p)
+		} else {
+			println("Unknown IPv4 protocol number", p.IPv4.NextProtoID)
+			println("TEST FAILED")
+		}
+	} else if p.Ether.EtherType == packet.SwapBytesUint16(packet.IPV6Number) {
+		if p.IPv6.Proto == packet.UDPNumber {
+			p.UDP.DgramCksum = packet.CalculateIPv6UDPChecksum(p)
+		} else if p.IPv6.Proto == packet.TCPNumber {
+			p.TCP.Cksum = packet.CalculateIPv6TCPChecksum(p)
+		} else {
+			println("Unknown IPv6 protocol number", p.IPv6.Proto)
+			println("TEST FAILED")
+		}
+	} else {
+		println("Unknown packet EtherType", p.Ether.EtherType)
+		println("TEST FAILED")
+	}
+}

aregm/nff-go

low/low.go
@@ -12,7 +12,8 @@ extern void recv(uint8_t port, uint16_t queue, struct rte_ring *, uint8_t coreId
 extern void send(uint8_t port, uint16_t queue, struct rte_ring *, uint8_t coreId);
 extern void stop(struct rte_ring *);
 extern void initCPUSet(uint8_t coreId, cpu_set_t* cpuset);
-extern int port_init(uint8_t port, uint16_t receiveQueuesNumber, uint16_t sendQueuesNumber, struct rte_mempool *mbuf_pool, struct ether_addr *addr);
+extern int port_init(uint8_t port, uint16_t receiveQueuesNumber, uint16_t sendQueuesNumber, struct rte_mempool *mbuf_pool,
+    struct ether_addr *addr, bool hwtxchecksum);
 extern struct rte_mempool * createMempool(uint32_t num_mbuf, uint32_t mbuf_cache_size);
 extern int directStop(int pkts_for_free_number, struct rte_mbuf ** buf);
 extern char ** makeArgv(int n);
@@ -119,6 +120,71 @@ func TrimMbuf(m *Mbuf, length uint) bool {
 	m.data_len -= C.uint16_t(length)
 	m.pkt_len -= C.uint32_t(length)
 	return true
+}
+
+func SetTXIPv4OLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_IP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (1 << 54) | (1 << 55)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv4UDPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_UDP_CKSUM | PKT_TX_IP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (3 << 52) | (1 << 54) | (1 << 55)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv4TCPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_TCP_CKSUM | PKT_TX_IP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (1 << 52) | (1 << 54) | (1 << 55)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv6UDPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_UDP_CKSUM | PKT_TX_IPV6
+	mb.ol_flags = (3 << 52) | (1 << 56)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
+}
+
+func SetTXIPv6TCPOLFlags(mb *Mbuf, l2len, l3len uint32) {
+	// PKT_TX_TCP_CKSUM | PKT_TX_IPV4
+	mb.ol_flags = (1 << 52) | (1 << 56)
+	mb.anon3[0] = uint8((l2len & 0x7f) | ((l3len & 1) << 7))
+	mb.anon3[1] = uint8(l3len >> 1)
+	mb.anon3[2] = 0
+	mb.anon3[3] = 0
+	mb.anon3[4] = 0
+	mb.anon3[5] = 0
+	mb.anon3[6] = 0
+	mb.anon3[7] = 0
 }
 
 // These constants are used by packet package to parse protocol headers
@@ -402,7 +468,7 @@ func GetPortsNumber() int {
 	return int(C.rte_eth_dev_count())
 }
 
-func CreatePort(port uint8, receiveQueuesNumber uint16, sendQueuesNumber uint16) {
+func CreatePort(port uint8, receiveQueuesNumber uint16, sendQueuesNumber uint16, hwtxchecksum bool) {
 	addr := make([]byte, C.ETHER_ADDR_LEN)
 	var mempool *C.struct_rte_mempool
 	if receiveQueuesNumber != 0 {
@@ -412,7 +478,7 @@ func CreatePort(port uint8, receiveQueuesNumber uint16, sendQueuesNumber uint16)
 		mempool = nil
 	}
 	if C.port_init(C.uint8_t(port), C.uint16_t(receiveQueuesNumber), C.uint16_t(sendQueuesNumber),
-		mempool, (*C.struct_ether_addr)(unsafe.Pointer(&(addr[0])))) != 0 {
+		mempool, (*C.struct_ether_addr)(unsafe.Pointer(&(addr[0]))), C._Bool(hwtxchecksum)) != 0 {
 		common.LogError(common.Initialization, "Cannot init port ", port, "!")
 	}
 	t := hex.Dump(addr)

test/stability/test_cksum/part2.go
@@ -1,0 +1,70 @@
+// Copyright 2017 Intel Corporation.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/intel-go/yanff/flow"
+	"github.com/intel-go/yanff/packet"
+
+	"github.com/intel-go/yanff/test/stability/test_cksum/test_common"
+)
+
+var hwol bool
+
+// Main function for constructing packet processing graph.
+func main() {
+	var inport, outport uint
+
+	flag.BoolVar(&hwol, "hwol", false, "Use Hardware offloading for TX checksums calculation")
+	flag.UintVar(&inport, "inport", 0, "Input port number")
+	flag.UintVar(&outport, "outport", 1, "Output port number")
+	flag.Parse()
+
+	// Init YANFF system
+	config := flow.Config{
+		CPUCoresNumber: 16,
+		HWTXChecksum:   hwol,
+	}
+	flow.SystemInit(&config)
+
+	// Receive packets from zero port. Receive queue will be added automatically.
+	inputFlow := flow.SetReceiver(uint8(inport))
+	flow.SetHandler(inputFlow, fixPacket, nil)
+	flow.SetSender(inputFlow, uint8(outport))
+
+	// Begin to process packets.
+	flow.SystemStart()
+}
+
+func fixPacket(pkt *packet.Packet, context flow.UserContext) {
+	if !test_common.CheckPacketChecksums(pkt) {
+		println("TEST FAILED")
+	}
+
+	offset := pkt.ParseL4Data()
+	if offset < 0 {
+		println("ParseL4 returned negative value", offset)
+		println("TEST FAILED")
+		return
+	}
+
+	ptr := (*test_common.Packetdata)(pkt.Data)
+	if ptr.F2 != 0 {
+		fmt.Printf("Bad data found in the packet: %x\n", ptr.F2)
+		println("TEST FAILED")
+		return
+	}
+
+	ptr.F2 = ptr.F1
+
+	if hwol {
+		packet.SetHWCksumOLFlags(pkt)
+	} else {
+		test_common.CalculateChecksum(pkt)
+	}
+}

test/stability/test_cksum/test_common/test1_common.go
@@ -1,0 +1,93 @@
+// Copyright 2017 Intel Corporation.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package test_common
+
+import (
+	"github.com/intel-go/yanff/common"
+	"github.com/intel-go/yanff/packet"
+)
+
+type Packetdata struct {
+	F1, F2 uint64
+}
+
+func CheckPacketChecksums(p *packet.Packet) bool {
+	status := false
+
+	if p.Ether.EtherType == packet.SwapBytesUint16(common.IPV4Number) {
+		l3status := true
+		if p.IPv4.HdrChecksum != packet.CalculateIPv4Checksum(p) {
+			println("IPv4 checksum mismatch")
+			l3status = false
+		}
+
+		if p.IPv4.NextProtoID == common.UDPNumber {
+			csum := packet.CalculateIPv4UDPChecksum(p)
+			if p.UDP.DgramCksum != csum {
+				println("IPv4 UDP datagram checksum mismatch", p.UDP.DgramCksum, "should be", csum)
+			} else {
+				status = l3status
+			}
+		} else if p.IPv4.NextProtoID == common.TCPNumber {
+			csum := packet.CalculateIPv4TCPChecksum(p)
+			if p.TCP.Cksum != csum {
+				println("IPv4 TCP checksum mismatch", p.TCP.Cksum, "should be", csum)
+			} else {
+				status = l3status
+			}
+		} else {
+			println("Unknown IPv4 protocol number", p.IPv4.NextProtoID)
+		}
+	} else if p.Ether.EtherType == packet.SwapBytesUint16(common.IPV6Number) {
+		if p.IPv6.Proto == common.UDPNumber {
+			csum := packet.CalculateIPv6UDPChecksum(p)
+			if p.UDP.DgramCksum != csum {
+				println("IPv6 UDP datagram checksum mismatch:", p.UDP.DgramCksum, "should be", csum)
+			} else {
+				status = true
+			}
+		} else if p.IPv6.Proto == common.TCPNumber {
+			csum := packet.CalculateIPv6TCPChecksum(p)
+			if p.TCP.Cksum != csum {
+				println("IPv6 TCP datagram checksum mismatch", p.TCP.Cksum, "should be", csum)
+			} else {
+				status = true
+			}
+		} else {
+			println("Unknown IPv6 protocol number", p.IPv6.Proto)
+		}
+	} else {
+		println("Unknown packet EtherType", p.Ether.EtherType)
+	}
+
+	return status
+}
+
+func CalculateChecksum(p *packet.Packet) {
+	if p.Ether.EtherType == packet.SwapBytesUint16(common.IPV4Number) {
+		p.IPv4.HdrChecksum = packet.CalculateIPv4Checksum(p)
+
+		if p.IPv4.NextProtoID == common.UDPNumber {
+			p.UDP.DgramCksum = packet.CalculateIPv4UDPChecksum(p)
+		} else if p.IPv4.NextProtoID == common.TCPNumber {
+			p.TCP.Cksum = packet.CalculateIPv4TCPChecksum(p)
+		} else {
+			println("Unknown IPv4 protocol number", p.IPv4.NextProtoID)
+			println("TEST FAILED")
+		}
+	} else if p.Ether.EtherType == packet.SwapBytesUint16(common.IPV6Number) {
+		if p.IPv6.Proto == common.UDPNumber {
+			p.UDP.DgramCksum = packet.CalculateIPv6UDPChecksum(p)
+		} else if p.IPv6.Proto == common.TCPNumber {
+			p.TCP.Cksum = packet.CalculateIPv6TCPChecksum(p)
+		} else {
+			println("Unknown IPv6 protocol number", p.IPv6.Proto)
+			println("TEST FAILED")
+		}
+	} else {
+		println("Unknown packet EtherType", p.Ether.EtherType)
+		println("TEST FAILED")
+	}
+}