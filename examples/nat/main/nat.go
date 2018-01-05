55f5753fb8162658e7b58b4ac51fa862fd3ac851
Gregory Shimansky <gregory.shimansky@intel.com>
2018-01-05 13:49:28 -0600

    Improved VM configs to run NAT example

--- examples/nat/main/nat.go
+++ examples/nat/main/nat.go
@@ -24,7 +24,7 @@ func CheckFatal(err error) {
 
 func main() {
 	// Parse arguments
-	cores := flag.String("cores", "0-43", "Specify CPU cores to use")
+	cores := flag.String("cores", "", "Specify CPU cores to use")
 	configFile := flag.String("config", "config.json", "Specify config file name")
 	flag.BoolVar(&nat.CalculateChecksum, "csum", true, "Specify whether to calculate checksums in modified packets")
 	flag.BoolVar(&nat.HWTXChecksum, "hwcsum", true, "Specify whether to use hardware offloading for checksums calculation (requires -csum)")
