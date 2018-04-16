02e6b3a51b54a33e74f267687e0731c047cfdb6d
Gregory Shimansky <gregory.shimansky@intel.com>
2018-04-16 13:51:03 -0500

    Switched vagrant script to work with libvirt
    An attempt has been made to preserve functioning VirtualBox
    provisioning, but it wasn't tested. Updated getports script and README
    with instructions on how to create reference set of VMs using libvirt.

--- vagrant/README.md
+++ vagrant/README.md
@@ -1,16 +1,11 @@
-To execute vagrant with this Vagrantfile it is recommended to use
-VirtualBox 5.2 or higher and vagrant 2.0.1 or higher. Additionally, if
-you use http proxy, it is necessary to install vagrant proxyconf
-plugin.
-
-Use this page to add VirtualBox repository to your Linux installation
-https://www.virtualbox.org/wiki/Linux_Downloads
+For VM specific section see below. Libvirt is the default VM now and
+it requires an additional vagrant plugin.
 
 Download Vagrant installation package from this page
 https://www.vagrantup.com/downloads.html. For Ubuntu use Debian .deb
 file and install it with command
 
-        sudo dpkg -i vagrant_2.0.1_x86_64.deb
+        sudo dpkg -i vagrant_2.0.3_x86_64.deb
 
 A plugin is required to restart VMs after provisioning:
 
@@ -48,3 +43,39 @@ single link: VM-VM-VM VM-VM.
 See wiki page https://github.com/intel-go/nff-go/wiki/NAT-example for
 instructions on how to configure virtual machines to run NFF-GO NAT
 example.
+
+## Libvirt/Qemu/KVM specific secion
+
+It is necessary to install libvirt vagrant plugin like this:
+
+    vagrant plugin install vagrant-libvirt
+
+Current setup creates VM in storage pool named 'images'. To create
+such pool use commands like this:
+
+    virsh pool-define-as images dir --target /localdisk/libvirt
+    virsh pool-start images
+
+VMs are connected via virtual networks which are created as UDP
+tunnes. For this it is necessary to allocate a sufficient number of
+UDP ports on localhost. Default port starting number is 12345. After
+this port VMs use ports continuously with total number equal to
+(VM_LINKS_NUMBER+1)*VM_TOTAL_NUMBER. If any of these ports are already
+occupied (e.g. some other use already created VMs with this starting
+port number), some network connections will not work. To change
+starting port number you can define VM_TUNNEL_PORT_BASE variable
+before using 'vagrant up' command.
+
+## VirtualBox specific section
+
+In our experience VirtualBox hangs and crashes too often. Its Intel
+network cards emulation doesn't support VLAN tags. That is why we had
+to switch to Libvirt/Qemu/KVM alternative.
+
+To execute vagrant with this Vagrantfile it is recommended to use
+VirtualBox 5.2 or higher and vagrant 2.0.1 or higher. Additionally, if
+you use http proxy, it is necessary to install vagrant proxyconf
+plugin.
+
+Use this page to add VirtualBox repository to your Linux installation
+https://www.virtualbox.org/wiki/Linux_Downloads
--- vagrant/getports.sh
+++ vagrant/getports.sh
@@ -3,8 +3,8 @@
 if (( "$#" != 1 ))
 then
     echo Usage: \$\(./getports.sh \<port number\>\)
-    echo Script generates \$sp0, \$sp1, etc variables if specified port is 22, and generates
-    echo \$dp0, \$dp1, etc if specified port is 2375.
+    echo Script generates \$sc0, \$sc1, etc variables if specified port is 22, and generates
+    echo \$da0, \$da1, etc if specified port is 2375.
     echo If specified port is 2375, script also generates NFF_GO_HOSTS variable.
     echo
     echo If VM_TOTAL_NUMBER is set, it is used as a number of VMs, otherwise 2 is used.
@@ -13,18 +13,6 @@ then
 fi
 
 port_number=$1
-
-if [ "${port_number}" == 22 ]
-then
-   pp="sp"
-elif [ "${port_number}" == 2375 ]
-then
-    hosts=""
-    pp="dp"
-else
-    echo Unknown port specified ${port_number}
-    exit 1
-fi
 
 if [ -z "${VM_NAME}" ]
 then
@@ -35,26 +23,59 @@ fi
 
 if [ -z "${VM_TOTAL_NUMBER}" ]
 then
-    number=2
+    number=3
 else
     number=${VM_TOTAL_NUMBER}
 fi
 
-for (( i=0; i<${number}; i++ ))
-do
-    port=$(vagrant port --guest ${port_number} ${vm_prefix}${i})
-    echo export ${pp}${i}=${port}
-    if [ "${port_number}" == 2375 ]
-    then
-        if (( i>0 ))
+if [ "${port_number}" == 22 ]
+then
+   pp="sc"
+   vagrant ssh-config > $(pwd)/config.ssh
+   for (( i=0; i<${number}; i++ ))
+   do
+       echo export ${pp}${i}=$(pwd)/config.ssh
+   done
+elif [ "${port_number}" == 2375 ]
+then
+    hosts=""
+    np_hosts=""
+    pp="da"
+    status=$(vagrant status)
+    for (( i=0; i<${number}; i++ ))
+    do
+        vm_name=${vm_prefix}${i}
+        if echo "${status}" | grep ${vm_name} | grep -q libvirt
         then
-            hosts=${hosts},
+            config=$(vagrant ssh-config ${vm_name})
+            address=$(echo "${config}" | grep HostName | cut -d " " -f 4)
+            port=${port_number}
+        elif echo "${status}" | grep ${vm_name} | grep -q virtualbox
+        then
+            address=localhost
+            port=$(vagrant port --guest ${port_number} ${vm_name})
+        else
+            echo Unknown provider for VM ${vm_name}
+            break
         fi
-        hosts=${hosts}localhost:${port}
-    fi
-done
+
+        echo export ${pp}${i}=${address}:${port}
+        if (( i==0 ))
+        then
+            hosts=${address}:${port}
+            np_hosts=${address}
+        else
+            hosts=${hosts},${address}:${port}
+            np_hosts=${np_hosts},${address}
+        fi
+    done
+else
+    echo Unknown port specified ${port_number}
+    exit 1
+fi
 
 if [ "${port_number}" == 2375 ]
 then
     echo export NFF_GO_HOSTS=${hosts}
+    echo export no_proxy=${np_hosts}
 fi
