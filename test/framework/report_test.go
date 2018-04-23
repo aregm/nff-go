100f59a4e127fd8e57fc77210a79f996b7e975f6
Maria Bulatova <maria.bulatova@intel.com>
2018-04-23 18:15:40 +0300

    Update latency test, add to test framework
    - Calculate median, average and stddev latency
    - Add timout to stabilize speed
    - Use FastGenerator
    - Remove latency-stub.go as unnecessary (perf_light and others should be
    used as part2)
    - Add more tests to latency.json
    - Add noscheduler flag
    - Add latency test type and latency report

--- test/framework/report_test.go
+++ test/framework/report_test.go
@@ -86,6 +86,8 @@ func testManyApps(t *testing.T, testtype TestType) {
 				appConfig[iii].Type = TestAppPktgen
 			} else if iii == 0 && testtype == TestTypeApacheBenchmark {
 				appConfig[iii].Type = TestAppApacheBenchmark
+			} else if iii == 0 && testtype == TestTypeLatency {
+				appConfig[iii].Type = TestAppLatency
 			} else {
 				appConfig[iii].Type = TestAppGo
 			}
@@ -132,6 +134,10 @@ func testManyApps(t *testing.T, testtype TestType) {
 				apps[iii].abs = &ApacheBenchmarkStats{
 					Stats: [4]float32{111.111, 222.222, 333.333, 444.444},
 				}
+			} else if appConfig[iii].Type == TestAppLatency {
+				apps[iii].lats = &LatencyStats{
+					Stats: [5]float32{111.111, 1000000, 222.222, 333.333, 444.444},
+				}
 			} else { // appConfig[iii].Type == TestAppGo
 				apps[iii].CoresStats = make([]CoresInfo, NUM_MEASUREMENTS)
 
@@ -170,6 +176,13 @@ func testManyApps(t *testing.T, testtype TestType) {
 					break
 				}
 			}
+		} else if testtype == TestTypeLatency {
+			for iii := range apps {
+				if apps[iii].config.Type == TestAppLatency {
+					tests[jjj].LatStats = apps[iii].lats
+					break
+				}
+			}
 		}
 	}
 
@@ -186,4 +199,8 @@ func TestScenarioManyApps(t *testing.T) {
 
 func TestApacheBenchmarkManyApps(t *testing.T) {
 	testManyApps(t, TestTypeApacheBenchmark)
+}
+
+func TestLatencyManyApps(t *testing.T) {
+	testManyApps(t, TestTypeLatency)
 }
