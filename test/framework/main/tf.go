547ebb32df1a1557edda442a05e6f046a727c255
Gregory Shimansky <gregory.shimansky@intel.com>
2018-05-09 13:39:58 -0500

    Added variables to test framework and other improvements
    Variables allow arbitrary string replacement in tests command
    lines. New section of test configuration now defines default variable
    values, other variables with values may be added or overridden from
    command line.
    
    Added -test switch to allow selecting only one/several tests from
    configuration file.
    
    Improved pass/fail diagnostics.
    
    The first experimental step of converting configuration files is done
    on stability.json checksum tests.

--- test/framework/main/tf.go
+++ test/framework/main/tf.go
@@ -13,6 +13,8 @@ import (
 )
 
 var hl test.HostsList
+var vl test.VariablesList = test.VariablesList{}
+var tl test.TestsList
 
 func main() {
 	test.SetLogLevel(test.LogDebugLvl)
@@ -20,13 +22,15 @@ func main() {
 	var directory string
 
 	flag.Var(&hl, "hosts", "Comma-separated list of `host:port,host:port...host:port` pairs to override settings in config file, e.g. \"-hosts host1:2375,host2:2376\".")
+	flag.Var(&vl, "var", "Variables listed in form NAME=VALUE. Variables are replaced in tests command lines in form NAME substring is replaced with VALUE. If whole paramenters becomes an empty string it is deleted entirely from command line. Multiple -var flags are allowed. Command line replacement order is not defined.")
+	flag.Var(&tl, "test", "Testcase regexp to execute. Test is executed if regexp matches test name substring. Multiple -test flags are allowed. If no -test flag is specified, all testcases in config file are executed.")
 	flag.StringVar(&configFile, "config", "config.json", "Name of config file to use")
 	flag.StringVar(&directory, "directory", "", "Use `directory` to output log files instead of timestamp")
 	flag.BoolVar(&test.NoDeleteContainersOnExit, "nodelete", false, "Do not remove containers after tests finish")
 	flag.Parse()
 
 	// Read config
-	config, err := test.ReadConfig(configFile, hl)
+	config, err := test.ReadConfig(configFile, hl, vl)
 	if err != nil {
 		test.LogPanic(err)
 	}
@@ -41,6 +45,6 @@ func main() {
 	}
 
 	// Start test execution
-	status := config.RunAllTests(directory)
+	status := config.RunAllTests(directory, tl)
 	os.Exit(status)
 }
