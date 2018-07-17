1a3175809e079909867ff20111d65a423ed1a63c
Vinod Borole <vinodborole@users.noreply.github.com>
2018-07-17 18:29:10 +0530

    Revert "NGIC|NFF : 1] NGIC DP implementation w/S11 2] ADC, SDF, PCC static ru…"

--- ngic/get-dependancies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-go get github.com/tkanos/gonfig
-go get gopkg.in/ini.v1
--- ngic/kni_ifcfg/kni-SGIdevcfg.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source ../config/dp_config.cfg
-ifconfig $DL_IFACE
-ifconfig $DL_IFACE $SGI_IP/24
-ifconfig $DL_IFACE
-
