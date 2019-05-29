c3a00e60a6940c13b54f7310bf5fcb55d33bb1d9
Gregory Shimansky <gregory.shimansky@intel.com>
2019-05-29 12:30:12 -0700

    Fixed byte order when converting IPv4 address to net.IP

--- types/ipv4.go
+++ types/ipv4.go
@@ -75,5 +75,5 @@ func NetIPToIPv4(ip net.IP) (IPv4Address, error) {
 
 // IPv4ToNetIP converts IPv4 to net.IP address.
 func IPv4ToNetIP(ip IPv4Address) net.IP {
-	return net.IPv4(byte(ip>>24), byte(ip>>16), byte(ip>>8), byte(ip))
+	return net.IPv4(byte(ip), byte(ip>>8), byte(ip>>16), byte(ip>>24))
 }
